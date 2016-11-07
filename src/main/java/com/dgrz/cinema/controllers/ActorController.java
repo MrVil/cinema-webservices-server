9bc8418f5d779d5577ccb089833fe53b1b4b987f
zominym <marc.zominy@etu.univ-lyon1.fr>
2016-11-07 09:46:05 +0100

    added routes in controllers and renamed everythig in english

--- src/main/java/com/dgrz/cinema/controllers/ActorController.java
+++ src/main/java/com/dgrz/cinema/controllers/ActorController.java
@@ -1,7 +1,41 @@
 package com.dgrz.cinema.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * Created by atanakar on 07/11/16.
  */
+
+@Controller
 public class ActorController {
+
+    @RequestMapping(path = "/api/actor/{id}", method = RequestMethod.GET)
+    public Actor getActor(@RequestParam(value="id") int id){
+
+        return null;
+    }
+
+    @RequestMapping(path = "/api/actors/", method = RequestMethod.GET)
+    public List<Actor> getActors(){
+
+        return null;
+    }
+
+    @RequestMapping(path = "/api/actor/{id}", method = RequestMethod.PUT)
+    public void updateActor(@RequestParam(value="id") int id){
+
+        return;
+    }
+
+
+    @RequestMapping(path = "/api/actor/{id}", method = RequestMethod.DELETE)
+    public void deleteActor(@RequestParam(value="id") int id){
+
+        return;
+    }
+
+
 }
--- src/main/java/com/dgrz/cinema/controllers/FilmController.java
+++ src/main/java/com/dgrz/cinema/controllers/FilmController.java
@@ -1,7 +1,40 @@
 package com.dgrz.cinema.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * Created by atanakar on 07/11/16.
  */
-public class FilmController {
+
+@Controller
+public class MovieController {
+
+    @RequestMapping(path = "/api/movie/{id}", method = RequestMethod.GET)
+    public Movie getMovie(@RequestParam(value="id") int id){
+
+        return null;
+    }
+
+    @RequestMapping(path = "/api/movies/", method = RequestMethod.GET)
+    public List<Movie> getMovies(){
+
+        return null;
+    }
+
+    @RequestMapping(path = "/api/movie/{id}", method = RequestMethod.PUT)
+    public void updateMovie(@RequestParam(value="id") int id){
+
+        return;
+    }
+
+    @RequestMapping(path = "/api/movie/{id}", method = RequestMethod.DELETE)
+    public void deleteMovie(@RequestParam(value="id") int id){
+
+        return;
+    }
+
+
 }
